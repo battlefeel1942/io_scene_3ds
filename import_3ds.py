5398350aae0ffb07e464813d14c83a9dfb96ed4c
battlefeel1942 <battlefeel1942@gmail.com>
2023-06-29 15:42:01 +1200

    Verbose logging of UVs

--- import_3ds.py
+++ import_3ds.py
@@ -360,11 +360,18 @@ def process_next_chunk(file, previous_chunk, importedObjects, IMAGE_SEARCH):
                     if v3 == 0:
                         v1, v2, v3 = v3, v1, v2
 
-                    uvl[pl.loop_start].uv = contextMeshUV[v1 * 2: (v1 * 2) + 2]
-                    uvl[pl.loop_start +
-                        1].uv = contextMeshUV[v2 * 2: (v2 * 2) + 2]
-                    uvl[pl.loop_start +
-                        2].uv = contextMeshUV[v3 * 2: (v3 * 2) + 2]
+                    # New debug print
+                    print(
+                        f"Face index: {fidx}, Face: {face}, Vertices: {v1, v2, v3}")
+
+                    uv1 = contextMeshUV[v1 * 2: (v1 * 2) + 2]
+                    uv2 = contextMeshUV[v2 * 2: (v2 * 2) + 2]
+                    uv3 = contextMeshUV[v3 * 2: (v3 * 2) + 2]
+                    print(f"UVs: {uv1, uv2, uv3}")  # New debug print
+
+                    uvl[pl.loop_start].uv = uv1
+                    uvl[pl.loop_start + 1].uv = uv2
+                    uvl[pl.loop_start + 2].uv = uv3
                     # always a tri
 
         bmesh.validate()
